ced5be9f370621dd0d1bd9b6b7f72d2263d32c9b
Davide Francesco Merico <hds619@gmail.com>
2017-01-08 19:42:14 +0100

    Removed Material-UI dep

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/Layout';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin ();
-
-ReactDOM.render (
-  <MuiThemeProvider>
-    <Layout />
-  </MuiThemeProvider>,
-  document.getElementById ('container')
-);
+ReactDOM.render (<Layout />, document.getElementById ('container'));
