ddc1d8b9e8d5012a56c5675c2d6532bef17e005b
Davide Francesco Merico <hds619@gmail.com>
2017-01-08 12:08:30 +0100

    Add ejs views

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 "use strict";
 
 const express = require ('express');
+const path = require ('path');
 const fs = require ('fs');
 const app = new express ();
 
-const build_err = (err) => {
-  return '<html><head><title>500 Error</title></head><body>'
-    + '<h1>500 Internal Error</h1>'
-    + '<p>' + err + '</p>'
-    + '</body></html>';
-};
+app.set ('view engine', 'ejs');
+app.set ('views', path.join (__dirname, 'src/views'));
+app.use (express.static ('dist'));
 
-app.get ('/', (req, res) => {
-  fs.readFile ('dist/index.html', 'utf8', (err, data) => {
-    if ( err ) {
-      res.writeHead (500, { 'Content-Type': 'text/html' });
-      res.end (build_err (err));
-      return console.warn (err);
-    }
+app.get ('/', (req, res) => res.render ('index', {
+  loading: '<div class="loading"></div>',
+  loading_info: '<!-- Loaded time: ' + (new Date ().getTime ()) + ' -->'
+}));
 
-    res.writeHead (200, { 'Content-Type': 'text/html' });
-    res.end (data);
-  });
-});
-
-app.listen (process.env.PORT || 3000, err => err => {
+app.listen (process.env.PORT || 3000, err => {
   if ( err )
     return console.error (err);
 
