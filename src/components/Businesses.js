252358945aaa1422665788e6b07402231803acd3
Davide Francesco Merico <hds619@gmail.com>
2017-01-12 20:18:04 +0100

    Businesses component

---
+++ src/components/Businesses.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Businesses = React.createClass ({
+  render () {
+    return (
+      <div>
+        {this.props.children}
+
+        { !this.props.loading ?
+          <button onClick={this.props.loadMore} className="load-more">
+            Load more business
+          </button> : '' }
+      </div>
+    );
+  }
+});
+
+export default Businesses;
