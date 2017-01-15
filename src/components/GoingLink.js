0dbe3eac4fb88ad8c07734bd7b933ea85c1ecb3b
Davide Francesco Merico <hds619@gmail.com>
2017-01-15 12:27:48 +0100

    Add GoingLink component

---
+++ src/components/GoingLink.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { mapStateToProps } from '../store.js';
+
+const GoingLink = React.createClass ({
+  getInitialState () {
+    return { loaded: false, usersGoing: 0 };
+  },
+  componentDidMount () {
+    this.props.dispatch ({
+      type: 'getJSON',
+      url: '/users-going',
+      data: {
+        id: this.props.id
+      },
+      callback: (result) => {
+        if ( result.error )
+          return console.error (result.error);
+
+        this.setState ({ loaded: true, usersGoing: result.usersGoing });
+      }
+    });
+  },
+  render () {
+    return (
+      <a onClick={this.toggleGoing}>
+        {this.state.loaded ?
+          {this.state.usersGoing + ' Going'} :
+          <div className="loading-small"></div>}
+      </a>
+    );
+  },
+  toggleGoing () {
+    console.log ('Check ' + this.props.id);
+  }
+});
+
+const StoreGoingLink = connect(mapStateToProps)(GoingLink);
+
+export default StoreGoingLink;
